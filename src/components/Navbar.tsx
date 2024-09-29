37d3c340eb1e2932d4c366f866f4e8d261f7dee3
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2024-09-29 07:11:37 +0500

    Add Create item button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,143 +1,206 @@
-'use client';
-import React, { useState } from 'react';
-import { AppBar, Toolbar, Typography, IconButton, Drawer, List, Box, Card, Button } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import Link from 'next/link';
+"use client";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Drawer,
+  List,
+  Box,
+  Card,
+  Button,
+  Tooltip,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import AddIcon from "@mui/icons-material/Add";
+import Link from "next/link";
+import FoodItemModal from "./add-foodItem-modal";
 
 const sections = [
-    'Burgers',
-    'Fries',
-    'Sandwiches',
-    'Special Rolls',
-    'Soup',
-    'BBQ',
-    'Milkshakes',
-    'Cold Coffee',
-    'Beverages',
-    'Lemonades',
-    'Extra'
+  "Burgers",
+  "Fries",
+  "Sandwiches",
+  "Special Rolls",
+  "Soup",
+  "BBQ",
+  "Milkshakes",
+  "Cold Coffee",
+  "Beverages",
+  "Lemonades",
+  "Extra",
 ];
 
 const scrollbarStyles = {
-    '&::-webkit-scrollbar': {
-        width: '6px',
+  "&::-webkit-scrollbar": {
+    width: "6px",
+  },
+  "&::-webkit-scrollbar-track": {
+    background: "#a1f7a5",
+  },
+  "&::-webkit-scrollbar-thumb": {
+    backgroundColor: "#388e3c",
+    borderRadius: "10px",
+    "&:hover": {
+      backgroundColor: "#3cbb47",
     },
-    '&::-webkit-scrollbar-track': {
-        background: '#a1f7a5',
-    },
-    '&::-webkit-scrollbar-thumb': {
-        backgroundColor: '#388e3c',
-        borderRadius: '10px',
-        '&:hover': {
-            backgroundColor: '#3cbb47',
-        },
-    },
+  },
 };
 
 const Navbar: React.FC = () => {
-    const [mobileOpen, setMobileOpen] = useState(false);
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
-    const handleDrawerToggle = () => {
-        setMobileOpen(!mobileOpen);
-    };
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
 
-    const drawer = (
-        <Box
-            onClick={handleDrawerToggle}
+  const handleAddItem = (item: any) => {
+    setFoodItems((prevItems) => [...prevItems, item]);
+  };
+
+  const drawer = (
+    <Box
+      onClick={handleDrawerToggle}
+      sx={{
+        textAlign: "center",
+        py: 2,
+        overflowY: "auto",
+        height: "100vh",
+        backgroundColor: "#f5f5f5",
+        ...scrollbarStyles,
+      }}
+    >
+      <Typography
+        variant="h4"
+        sx={{ mb: 2 }}
+        color="#388e3c"
+        fontWeight={"semibold"}
+      >
+        FoodMart
+      </Typography>
+      <List>
+        {sections.map((section) => (
+          <Card
+            key={section}
             sx={{
-                textAlign: 'center',
-                py: 2,
-                overflowY: 'auto',
-                height: '100vh',
-                backgroundColor: '#f5f5f5',
-                ...scrollbarStyles,
+              margin: 2,
+              padding: 2,
+              boxShadow: 1,
+              "&:hover": {
+                boxShadow: 4,
+                transform: "scale(1.02)",
+                transition: "0.3s",
+              },
             }}
-        >
-            <Typography variant="h4" sx={{ mb: 2 }} color='#388e3c' fontWeight={'semibold'}>
-                FoodMart
+          >
+            <Typography variant="body1" align="center">
+              {section}
             </Typography>
-            <List>
-                {sections.map((section) => (
-                    <Card
-                        key={section}
-                        sx={{
-                            margin: 2,
-                            padding: 2,
-                            boxShadow: 1,
-                            '&:hover': {
-                                boxShadow: 4,
-                                transform: 'scale(1.02)',
-                                transition: '0.3s',
-                            },
-                        }}
-                    >
-                        <Typography variant="body1" align="center">
-                            {section}
-                        </Typography>
-                    </Card>
-                ))}
-            </List>
-        </Box>
-    );
+          </Card>
+        ))}
+      </List>
+    </Box>
+  );
 
-    return (
-        <>
-            <AppBar position="static" sx={{ backgroundColor: '#388e3c' }}>
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleDrawerToggle} sx={{ mr: 2 }}>
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h4" sx={{ flexGrow: 1 }}>
-                        FoodMart
-                    </Typography>
+  return (
+    <>
+      <AppBar position="static" sx={{ backgroundColor: "#388e3c" }}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h4" sx={{ flexGrow: 1 }}>
+            FoodMart
+          </Typography>
 
-                    <Link href="https://wa.me/+923151272630" passHref target="_blank">
-                        <Button
-                            sx={{
-                                backgroundColor: 'white',
-                                color: '#388e3c',
-                                '&:hover': {
-                                    backgroundColor: '#b4fab7',
-                                    scale: '1.1',
-                                },
-                                border: '1px solid #388e3c',
-                                borderRadius: '500px',
-                                mr: 2,
-                            }}
-                            variant="outlined"
-                            startIcon={<WhatsAppIcon sx={{ color: '#388e3c', }} />}
-                        >
-                            Chat Now
-                        </Button>
-                    </Link>
+          <Tooltip title="Chat with us on WhatsApp">
+            <Link href="https://wa.me/+923151272630" passHref target="_blank">
+              <Button
+                sx={{
+                  backgroundColor: "white",
+                  color: "#388e3c",
+                  "&:hover": {
+                    backgroundColor: "#b4fab7",
+                    scale: "1.1",
+                  },
+                  border: "1px solid #388e3c",
+                  borderRadius: "500px",
+                  mr: 2,
+                }}
+                variant="outlined"
+                startIcon={<WhatsAppIcon sx={{ color: "#388e3c" }} />}
+              >
+                Chat Now
+              </Button>
+            </Link>
+          </Tooltip>
 
-                    <Link href="/cart" passHref>
-                        <Button
-                            sx={{
-                                backgroundColor: 'white',
-                                color: '#388e3c',
-                                '&:hover': {
-                                    backgroundColor: '#b4fab7',
-                                    scale: '1.1',
-                                },
-                                border: '1px solid #388e3c',
-                                borderRadius: '500px',
-                                pl: 3,
-                            }}
-                            variant="outlined"
-                            startIcon={<ShoppingCartIcon sx={{ color: '#388e3c', }} />}
-                        >
-                        </Button>
-                    </Link>
-                </Toolbar>
-            </AppBar>
-            <Drawer open={mobileOpen} onClose={handleDrawerToggle} sx={{ '& .MuiDrawer-paper': { width: 300 } }}>
-                {drawer}
-            </Drawer>
-        </>
-    );
-}
+          <Tooltip title="Go to Cart">
+            <Link href="/cart" passHref>
+              <Button
+                sx={{
+                  backgroundColor: "white",
+                  color: "#388e3c",
+                  "&:hover": {
+                    backgroundColor: "#b4fab7",
+                    scale: "1.1",
+                  },
+                  border: "1px solid #388e3c",
+                  borderRadius: "500px",
+                  pl: 3,
+                  mr: 2,
+                }}
+                variant="outlined"
+                startIcon={<ShoppingCartIcon sx={{ color: "#388e3c" }} />}
+              ></Button>
+            </Link>
+          </Tooltip>
+
+          <Tooltip title="Add New Food Item">
+            <Button
+              sx={{
+                backgroundColor: "white",
+                color: "#388e3c",
+                "&:hover": {
+                  backgroundColor: "#b4fab7",
+                  scale: "1.1",
+                },
+                border: "1px solid #388e3c",
+                borderRadius: "500px",
+                pl: 3,
+              }}
+              variant="outlined"
+              startIcon={<AddIcon sx={{ color: "#388e3c" }} />}
+              onClick={() => setModalOpen(true)}
+            ></Button>
+          </Tooltip>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        sx={{ "& .MuiDrawer-paper": { width: 300 } }}
+      >
+        {drawer}
+      </Drawer>
+
+      <FoodItemModal
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+        onAddItem={handleAddItem}
+      />
+    </>
+  );
+};
 
 export default Navbar;
